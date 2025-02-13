384406b8cfa24d615280b81f1b0cd153081fe72e
TheCoderKamal <kamalsagpariya7@gmail.com>
2025-02-13 08:41:45 +0530

    commit admin project

---
+++ 14] adminPanelproject/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const port = 1234;
+const cookie = require("cookie-parser");
+
+const app = express();
+const db = require("./config/db");
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded());
+app.use("/", require("./routes/route"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use(cookie());
+
+app.listen(port, err => {
+    err ? console.log("Error : ", err) : console.log("Server is running on port : ", port)
+})
